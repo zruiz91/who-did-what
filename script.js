e83171e0bcb86a2f21c7beb56fd4e0cc3a7751d7
Zach Ruiz <powersbooth10@gmail.com>
2022-08-09 15:35:24 -0500

    linked js to html and moved 'show more' button code to script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const date = new Date();
-document.getElementById("date").innerHTML = date.toDateString();
+function moreLess() {
+    const elipses = document.getElementById("elipses");
+    const moreText = document.getElementById("more");
+    const btnText = document.getElementById("btn");
+
+    if (elipses.style.display === "none") {
+        elipses.style.display = "inline";
+        btnText.innerHTML = "Read More";
+        moreText.style.display = "none";
+    } else {
+        elipses.style.display = "none";
+        btnText.innerHTML = "Read Less"
+        moreText.style.display = "inline"
+    }
+}
